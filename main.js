1d5063472fbee40ff021fb20ba2cecb628b27aeb
Lind <lindjc19@gcc.edu>
2020-10-08 00:02:12 -0400

    Created three seperate arrays for meals and scarped information for each

--- main.js
+++ main.js
@@ -7,29 +7,6 @@ require('dotenv').config();
 
 // necessary functions
 
-// Deletes all objects with douplicate names in the given array.
-function removeDouplicates(a) {
-    let aNames = [];
-    let b = [];
-    let aNew = [];
-  
-    // put only the names from the objects in a into an array.
-    for (let i = 0; i < a.length; i++) {
-      aNames.push(a[i].name);
-    }
-    
-    // if the element is not a douplicate, we push the object into aNew.
-    for (let i = 0; i < aNames.length; i++) {
-      if (b.indexOf(aNames[i]) === -1) {
-        b.push(aNames[i]);
-        aNew.push(a[i]);
-      }
-    }
-  
-    return aNew;
-}
-
-
 
 const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
@@ -61,53 +38,98 @@ var bfFoodList;
     
 
     // Returns list of foods form menu page.
-    bfFoodList = await page.evaluate(() => {
-        let bTabOpen = document.getElementsByClassName('site-panel__daypart-accordion-btn');
-        if (bTabOpen[0].innerText != 'Collapse Daypart 1') {
-            bTabOpen[0].click();
-        }
+    hicksFoodLists = await page.evaluate(() => {
 
-        // hicksListLocation is the array of objects containing the HTML for each food elelment on the page
-        let hicksListLocation = document.querySelector('[id^="tab-content"]').getElementsByClassName('h4 site-panel__daypart-item-title');
-        let hicksBreakfastList = []
-
-        // loops through array of food objects and searches for if the foods are vegan, vegetarian or gluten free
-        // sets all variables in object which is then pushed to array hicksBreakfastList
-        for (let i = 0; i < hicksListLocation.length; i++) {
-            
-            if (hicksBreakfastList.name != undefined && !(hicksBreakfastList.name).includes(hicksListLocation[i].name)) {
-                let temp = { name: '', vegan: false, vegetarian: false, glutenFree: false};
-                temp.name = hicksListLocation[i].innerText;
-
-                if (hicksListLocation[i].innerHTML.includes('egan')) { temp.vegan = true };
-                if (hicksListLocation[i].innerHTML.includes('egetarian')) { temp.vegetarian = true };
-                if (hicksListLocation[i].innerHTML.includes('luten')) { temp.glutenFree = true };
-
-                hicksBreakfastList.push(temp);
+        // necessary functions:
+        // Deletes all objects with douplicate names in the given array.
+        function removeDouplicates(a) {
+            let aNames = [];
+            let b = [];
+            let aNew = [];
+          
+            // put only the names from the objects in a into an array.
+            for (let i = 0; i < a.length; i++) {
+              aNames.push(a[i].name);
             }
             
+            // if the element is not a douplicate, we push the object into aNew.
+            for (let i = 0; i < aNames.length; i++) {
+              if (b.indexOf(aNames[i]) === -1) {
+                b.push(aNames[i]);
+                aNew.push(a[i]);
+              }
+            }
+          
+            return aNew;
         }
 
-        // Clear duplicates from hicksBreakfastList array. 
+
+        // hicks meal food arrays.
+
+        let hicksBreakfastList = [];
+        let hicksLunchList = [];
+        let hicksDinnerList = [];
+        
+        let tabOpen = document.getElementsByClassName('site-panel__daypart-accordion-btn');
+        if (tabOpen[0].innerText != 'Collapse Daypart 1') { tabOpen[0].click() };
+        if (tabOpen[1].innerText != 'Collapse Daypart 1') { tabOpen[1].click() };
+        if (tabOpen[2].innerText != 'Collapse Daypart 1') { tabOpen[2].click() };
+
+        // hicks(meal)ListLocation are the arrays of objects containing the HTML for each food elelment for each meal section
+        let hicksBreakfastListLocation = document.querySelector('[id^="tab-content"]').getElementsByClassName('h4 site-panel__daypart-item-title');
+        let hicksLunchListLocation = document.querySelector('#lunch .site-panel__daypart-wrapper .site-panel__daypart-tabs .c-tabs .c-tab__content.site-panel__daypart-tab-content').getElementsByClassName('h4 site-panel__daypart-item-title');
+        let hicksDinnerListLocation = document.querySelector('#dinner .site-panel__daypart-wrapper .site-panel__daypart-tabs .c-tabs .c-tab__content.site-panel__daypart-tab-content').getElementsByClassName('h4 site-panel__daypart-item-title');
+        
+        // loops through all three arrays of food objects and searches for if the foods are vegan, vegetarian or gluten free
+        // sets all variables in object which is then pushed to array hicksBreakfastList
+        for (let i = 0; i < hicksBreakfastListLocation.length; i++) {
+            
+            let temp = { name: '', vegan: false, vegetarian: false, glutenFree: false};
+            temp.name = hicksBreakfastListLocation[i].innerText.trim();
+
+            if (hicksBreakfastListLocation[i].innerHTML.includes('egan')) { temp.vegan = true };
+            if (hicksBreakfastListLocation[i].innerHTML.includes('egetarian')) { temp.vegetarian = true };
+            if (hicksBreakfastListLocation[i].innerHTML.includes('luten')) { temp.glutenFree = true };
+
+            hicksBreakfastList.push(temp);
+        
+        }
+        for (let i = 0; i < hicksLunchListLocation.length; i++) {
+            let temp = { name: '', vegan: false, vegetarian: false, glutenFree: false};
+            temp.name = hicksLunchListLocation[i].innerText.trim();
+
+            if (hicksLunchListLocation[i].innerHTML.includes('egetarian')) { temp.vegetarian = true };
+            if (hicksLunchListLocation[i].innerHTML.includes('egan')) { temp.vegan = true };
+            if (hicksLunchListLocation[i].innerHTML.includes('luten')) { temp.glutenFree = true };
+
+            hicksLunchList.push(temp);
+        }
+        for (let i = 0; i < hicksDinnerListLocation.length; i++) {
+            let temp = { name: '', vegan: false, vegetarian: false, glutenFree: false};
+            temp.name = hicksDinnerListLocation[i].innerText.trim();
+
+            if (hicksDinnerListLocation[i].innerHTML.includes('egetarian')) { temp.vegetarian = true };
+            if (hicksDinnerListLocation[i].innerHTML.includes('egan')) { temp.vegan = true };
+            if (hicksDinnerListLocation[i].innerHTML.includes('luten')) { temp.glutenFree = true };
+
+            hicksDinnerList.push(temp);
+        }
+
+
+        // Clear duplicates from hicks food list arrays. 
         hicksBreakfastList = removeDouplicates(hicksBreakfastList);
+        hicksLunchList = removeDouplicates(hicksLunchList);
+        hicksDinnerList = removeDouplicates(hicksDinnerList);
 
 
-
-
-
-        return foodList;
+        return hicksBreakfastList;
     })
 
 
-
-
-
-
-
-    mailOptions.text = bfFoodList;
+    mailOptions.text = hicksFoodLists.toString();
     transporter.sendMail(mailOptions, function(err, data) {
         if (err) {
-            console.log("ERROR!!!");
+            console.log(err);
         } else {
             console.log("Email sent!");
         }
